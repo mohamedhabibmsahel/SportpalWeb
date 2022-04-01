bb2b3c25d9bc17c30eae3e950379a10e39f307cf
m'sahel mohamed habib <48933499+mohamedhabibmsahel@users.noreply.github.com>
2022-04-02 00:07:03 +0100

    add interfaces

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -6,14 +6,16 @@ import { Routes, RouterModule } from '@angular/router';
 import { ComponentsComponent } from './components/components.component';
 import { ProfileComponent } from './examples/profile/profile.component';
 import { SignupComponent } from './examples/signup/signup.component';
+import { SigninComponent } from './examples/signin/signin.component';
 import { LandingComponent } from './examples/landing/landing.component';
 import { NucleoiconsComponent } from './components/nucleoicons/nucleoicons.component';
 
 const routes: Routes =[
-    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: '', redirectTo: 'landing', pathMatch: 'full' },
     { path: 'home',             component: ComponentsComponent },
     { path: 'user-profile',     component: ProfileComponent },
     { path: 'signup',           component: SignupComponent },
+    { path: 'signin',           component: SigninComponent },
     { path: 'landing',          component: LandingComponent },
     { path: 'nucleoicons',      component: NucleoiconsComponent }
 ];
--- src/app/examples/examples.module.ts
+++ src/app/examples/examples.module.ts
@@ -6,6 +6,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { LandingComponent } from './landing/landing.component';
 import { ProfileComponent } from './profile/profile.component';
 import { SignupComponent } from './signup/signup.component';
+import { SigninComponent } from './signin/signin.component';
 
 @NgModule({
     imports: [
@@ -16,6 +17,7 @@ import { SignupComponent } from './signup/signup.component';
     declarations: [
         LandingComponent,
         SignupComponent,
+        SigninComponent,
         ProfileComponent
     ]
 })
